9d99a84aeb8ba184557fcd66713db0aff998a298
Sarah Johnson <sarahjohnson@Sarahs-MacBook-Air.local>
2021-04-28 08:50:44 -0700

    working on JS animation

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const darkOverlay = document.querySelector(".overlay");
 const hamburgerLnk = document.querySelectorAll(".hamburger ul li");
 const hamburgerLnkArr = Array.from(hamburgerLnk);
 
-// console.log(hamburgerLnkArr[3]);
 
 function handleMenuClick() {
     hamburgerDrop.style.width = "250px";
@@ -24,6 +23,33 @@ darkOverlay.addEventListener("mouseenter", handleSideNavClose);
 for (let x of hamburgerLnkArr) {
   addEventListener("click", handleSideNavClose);
 }
+
+
+const lines = document.querySelectorAll(".titles");
+const linesArr = Array.from(lines);
+
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
+
+for (let y of linesArr) {
+   if (window.addEventListener('scroll', function(isInViewport))) ===
+   return (
+      
+   )  
+}
+  // grab all the headings
+ // loop over them 
+ // for each one, check if they isInViewport is true for the element
+ // if it's true, trigger animation
+ 
 
 //stop listening for mouse leave at screen size 990 and up
 // const mediaQueryList = window.matchMedia("(min-width: 990px)");
