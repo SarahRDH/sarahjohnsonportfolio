cf69f27c7e87f91ed3cf39db825102ee7e5773fe
Sarah Johnson <sarahjohnson@Sarahs-MacBook-Air.local>
2021-04-28 14:51:28 -0700

    small change to button

--- script.js
+++ script.js
@@ -25,31 +25,31 @@ for (let x of hamburgerLnkArr) {
 }
 
 //playing line animation under titles of each section
-const lines = document.querySelectorAll(".titles");
-const linesArr = Array.from(lines);
+// const lines = document.querySelectorAll(".titles");
+// const linesArr = Array.from(lines);
 
-function showLines() { 
-  linesArr[i].style.display = "block";
-  console.log("showLines function works");
-}
+// function showLines() { 
+//   linesArr[i].style.display = "block";
+//   console.log("showLines function works");
+// }
 
-function isInViewport(element) {
-  var rect = element.getBoundingClientRect();
-  var html = document.documentElement;
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || html.clientHeight) &&
-    rect.right <= (window.innerWidth || html.clientWidth)
-  );
-}
+// function isInViewport(element) {
+//   var rect = element.getBoundingClientRect();
+//   var html = document.documentElement;
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || html.clientHeight) &&
+//     rect.right <= (window.innerWidth || html.clientWidth)
+//   );
+// }
 
-const doIt = isInViewport(showLines);
-for (i=0; i < linesArr.length; i++) {
-  linesArr[i].addEventListener("scroll", isInViewport); 
-  console.log("listening for scroll");  
-  showLines();
-}
+// const doIt = isInViewport(showLines);
+// for (i=0; i < linesArr.length; i++) {
+//   linesArr[i].addEventListener("scroll", isInViewport); 
+//   console.log("listening for scroll");  
+//   showLines();
+// }
 
 // function isInViewport(linesArr) {
 //   var rect = linesArr.getBoundingClientRect();
